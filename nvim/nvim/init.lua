e82a1cc7522c76c5f645eb151e1ed5556bf31513
Vivek R <123vivekr@gmail.com>
2023-06-30 22:36:54 +0530

    nvim: rewrite

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,4 @@
+require("vivek.plugins-setup")
+require("vivek.core.options")
+require("vivek.core.keymaps")
+require("vivek.core.colorscheme")
---
+++ nvim/nvim/lua/vivek/core/keymaps.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps
+---------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
